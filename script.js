1fa827a98591ef62f2c2d577890866fc30c79762
ferhat <ferhattahar46@gmail.com>
2024-04-28 21:27:03 +0100

    delete the unnecessary files

---
+++ script.js
@@ -0,0 +1,9 @@
+let test = "hello ilyes";
+console.log(test);
+console.log((100 - 50) * 2);
+if (test == "hello ilyes") {
+  console.log("if used");
+} else {
+  console.log("else used");
+}
+alert("it linked correctly!!");
